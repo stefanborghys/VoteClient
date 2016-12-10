097e3d51ce7c176a1e980dbdd4f8976188ee695b
Dieter <dieter.jordens@continuum.be>
2016-12-10 15:13:25 +0100

    Ward Stinissen changes

--- user/LogoutController.js
+++ user/LogoutController.js
@@ -1,8 +1,12 @@
 // Define the 'LogoutController' controller on the 'voteMachineApp' module
-voteMachineApp.controller('LogoutController', ['$scope', '$log', function($scope, $log) {
+voteMachineApp.controller('LogoutController', ['$scope', '$log', '$location', function($scope, $log) {
     "use strict";
 
     $scope.message = "Implement this ... and use the LogoutController";
 
-
+    $scope.logout = function() {
+        $log.debug('logout clicked');
+        Session.clear();
+        $location.path("/user_sign_in");
+    };
 }]);
